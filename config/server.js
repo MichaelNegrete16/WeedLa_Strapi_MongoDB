699a76b4d6c106982b5e15f98daa8f33d7f23b89
Jackson <michael.negrete16@gmail.com>
2022-03-18 17:25:19 -0500

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ead710753892c3ae9402775a0f8977d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4d9a94ab-8145-4138-a68d-913b515850d5'
+};
